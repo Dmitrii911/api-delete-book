ff2c0d5091ba7d8c9df45cc9c82bdae5c8fbde08
Dmitrii Oleinik <wedil@mail.ru>
2025-09-13 17:00:31 +0500

    add TestData

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,6 +1,6 @@
 package tests;
 
 public class TestData {
-    public static final String username = System.getProperty("username","test123456");
-    public static final String password = System.getProperty("password", "Test123456@");
+    public static final String username = System.getProperty("username");
+    public static final String password = System.getProperty("password");
 }
